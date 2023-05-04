0be5c1a08fed1f67e909d22a1bb8c5ca67a09305
Yeison Garzon <yeisongarzonn@gmail.com>
2023-05-04 17:47:15 -0500

    se agrego archivo js para menu responsivo

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -2,5 +2,5 @@ const icono = document.getElementById("icono_menu");
 const menu = document.getElementById("menu_ul");
 
 icono.addEventListener("click", () => {
-    menu.classList.toggle("menu_ul-show")
+    menu.classList.toggle("menu_ul-show");
 })
